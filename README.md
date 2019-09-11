1935c14296632a02c1fef7305f9b4ae171bbb04d
Vedad Sose <vedadsose@gmail.com>
2019-09-11 23:30:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# NPM CI Login
+
+Logs you into NPM on CI machines.
+
+Usage:
+```
+NPM_USERNAME=Snoop NPM_PASSWORD=Dogg NPM_EMAIL=snoop@420.com npm-ci-login
+```
+
+The tool also supports:
+
+`NPM_REGISTRY` - If you have a custom registry
+`NPM_SCOPE` - Organisation scope 
---
+++ index.js
@@ -0,0 +1,56 @@
+const { spawn } = require("child_process");
+
+const validate = ({ username, password, email }) => {
+  if (!username) {
+    console.log("Please provide username via NPM_USERNAME");
+    process.exit(1);
+  }
+
+  if (!password) {
+    console.log("Please provide password via NPM_USERNAME");
+    process.exit(1);
+  }
+
+  if (!email) {
+    console.log("Please provide email via NPM_USERNAME");
+    process.exit(1);
+  }
+};
+
+const login = props => {
+  const { username, password, email, scope, registry } = props;
+
+  validate(props);
+
+  const npmLogin = spawn(
+    "npm",
+    [
+      "login",
+      scope && `--scope=${scope}`,
+      registry && `--registry=${registry}`
+    ].filter(Boolean)
+  );
+
+  npmLogin.stderr.on("data", data => {
+    console.log("Error: ", data.toString("utf-8"));
+    process.exit(1);
+  });
+
+  npmLogin.on("close", () => process.exit(0));
+
+  npmLogin.stdin.write(username + "\n");
+  setTimeout(() => {
+    npmLogin.stdin.write(password + "\n");
+  }, 2000);
+  setTimeout(() => {
+    npmLogin.stdin.write(email + "\n");
+  }, 4000);
+};
+
+login({
+  username: process.env.NPM_USERNAME,
+  password: process.env.NPM_PASSWORD,
+  email: process.env.NPM_EMAIL,
+  scope: process.env.NPM_SCOPE,
+  registry: process.env.NPM_REGISTRY
+});
